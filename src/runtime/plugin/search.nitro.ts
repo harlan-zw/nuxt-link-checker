646e6ff8721eaeb4ac4c56a3a0e03c0ce764f90e
Harlan Wilton <harlan@harlanzw.com>
2023-08-29 12:23:39 +0200

    fix: lazy init fuzzy page search client
    Maybe fixes #14

--- src/runtime/plugin/search.nitro.ts
+++ src/runtime/plugin/search.nitro.ts
@@ -1,9 +1,13 @@
 import Fuse from 'fuse.js'
 import { defineNitroPlugin } from 'nitropack/dist/runtime/plugin'
 
-export default defineNitroPlugin(async (nitro) => {
-  const pages = await $fetch('/api/__link_checker__/links') as string[]
-  nitro._linkCheckerPageSearch = new Fuse(pages, {
-    threshold: 0.5,
-  })
+export default defineNitroPlugin((nitro) => {
+  setTimeout(() => {
+    $fetch('/api/__link_checker__/links').then((pages: string[]) => {
+      // @ts-expect-error context hack
+      nitro._linkCheckerPageSearch = new Fuse(pages, {
+        threshold: 0.5,
+      })
+    })
+  }, 200)
 })
