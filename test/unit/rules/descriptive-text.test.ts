20376c40937bfab3fd3eed458f4c501b4e8e12dc
harlan <harlan@harlanzw.com>
2024-10-30 01:17:19 +1100

    chore: missing tests

---
+++ test/unit/rules/descriptive-text.test.ts
@@ -0,0 +1,28 @@
+import type { RuleTestContext } from '../../../src/runtime/types'
+import { describe, expect, it } from 'vitest'
+import RuleDescriptiveLinkText from '../../../src/runtime/pure/inspections/descriptive-link-text'
+import { runRule } from './util'
+
+describe('rule descriptive text', () => {
+  it('works', () => {
+    const ctx = {
+      siteConfig: {
+        trailingSlash: true,
+      },
+      response: { status: 200, statusText: 'OK', headers: {} },
+      link: '/og-image/api/define-og-image',
+      textContent: 'defineOgImage',
+    } as RuleTestContext
+
+    expect(runRule(ctx, RuleDescriptiveLinkText())).toMatchInlineSnapshot(`
+      {
+        "error": [],
+        "fix": "/og-image/api/define-og-image",
+        "link": "/og-image/api/define-og-image",
+        "passes": true,
+        "textContent": "defineOgImage",
+        "warning": [],
+      }
+    `)
+  })
+})
