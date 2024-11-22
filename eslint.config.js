650b33adbe214bfbb9b8ef3cdbbfebb9e716abbf
harlan <harlan@harlanzw.com>
2024-11-22 17:01:44 +1100

    chore: bump deps

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,7 @@ export default antfu({
   rules: {
     'no-use-before-define': 'off',
     'node/prefer-global/process': 'off',
+    // TODO re-enable when not broken
+    'vue/no-empty-pattern': 'off',
   },
 })
