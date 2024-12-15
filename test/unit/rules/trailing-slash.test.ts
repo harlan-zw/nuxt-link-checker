64c44dbb6c945624a17d7c54da4ad84de5d0d569
Alexander Lichter <github@lichter.io>
2024-12-15 04:04:48 +0100

    fix: ignore fragment-only links (#49)

--- test/unit/rules/trailing-slash.test.ts
+++ test/unit/rules/trailing-slash.test.ts
@@ -33,4 +33,55 @@ describe('rule trailing-slash', () => {
       }
     `)
   })
+
+  it('works with fragment-only links', () => {
+    const ctx = {
+      siteConfig: {
+        trailingSlash: true,
+      },
+      response: { status: 200, statusText: 'OK', headers: {} },
+      link: '#abc',
+    } as RuleTestContext
+
+    expect(runRule(ctx, RuleTrailingSlash())).toMatchInlineSnapshot(`
+      {
+        "error": [],
+        "fix": "#abc",
+        "link": "#abc",
+        "passes": true,
+        "textContent": undefined,
+        "warning": [],
+      }
+    `)
+  })
+
+  it('works with links containing fragments', () => {
+    const ctx = {
+      siteConfig: {
+        trailingSlash: true,
+      },
+      response: { status: 200, statusText: 'OK', headers: {} },
+      link: '/a#abc',
+    } as RuleTestContext
+
+    expect(runRule(ctx, RuleTrailingSlash())).toMatchInlineSnapshot(`
+      {
+        "error": [],
+        "fix": "/a/#abc",
+        "link": "/a#abc",
+        "passes": false,
+        "textContent": undefined,
+        "warning": [
+          {
+            "fix": "/a/#abc",
+            "fixDescription": "Add trailing slash.",
+            "message": "Should have a trailing slash.",
+            "name": "trailing-slash",
+            "scope": "warning",
+            "tip": "Incorrect trailing slashes can cause duplicate pages in search engines and waste crawl budget.",
+          },
+        ],
+      }
+    `)
+  })
 })
