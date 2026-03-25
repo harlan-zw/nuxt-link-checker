412da245a4ef8207688b9ca23bafa9a894c5d58f
Harlan Wilton <harlan@harlanzw.com>
2026-03-25 18:49:52 +1100

    chore: lint

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -8,40 +8,6 @@ export default defineNuxtConfig({
     '../src/module',
     '@nuxtjs/sitemap',
     '@nuxt/content',
-
-    /**
-     * Start a sub Nuxt Server for developing the client
-     *
-     * The terminal output can be found in the Terminals tab of the devtools.
-     */
-    defineNuxtModule({
-      setup(_, nuxt) {
-        if (!nuxt.options.dev)
-          return
-
-        const subprocess = startSubprocess(
-          {
-            command: 'npx',
-            args: ['nuxi', 'dev', '--port', '3030'],
-            cwd: resolve(__dirname, '../client'),
-          },
-          {
-            id: 'nuxt-link-checker:client',
-            name: 'Nuxt Link Checker Client Dev',
-          },
-        )
-        subprocess.getProcess().stdout?.on('data', (data) => {
-          console.log(` sub: ${data.toString()}`)
-        })
-
-        process.on('exit', () => {
-          subprocess.terminate()
-        })
-
-        // process.getProcess().stdout?.pipe(process.stdout)
-        // process.getProcess().stderr?.pipe(process.stderr)
-      },
-    }),
   ],
 
   // @ts-expect-error untyped
